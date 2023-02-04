b62875993389d4e2054d24a415d5ad090ea5401d
ikebata_yuki <walkurepqrt@gmail.com>
2023-02-04 23:13:05 +0900

    entrypoint.shの修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,3 @@ rm -f /myapp/tmp/pids/server.pid
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
-
-if [ "$RAILS_ENV" = "production" ]; then
-bundle exec rails assets:clobber
-bundle exec rails assets:precompile
-bundle exec rails db:migrate
-fi

34df5cd35fe694580fa05d2b3c161a864d9ebc38
ikebata_yuki <walkurepqrt@gmail.com>
2023-02-04 18:41:14 +0900

    dockerの導入。railsアプリの作成。Reactの導入とHelloReactの表示まで

---
+++ app/javascript/application.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+function App() {
+  return (
+    <h1>Hello React!</h1>
+  )
+}
+
+const root = document.getElementById('root');
+if (!root) {
+  throw new Error('No root element');
+}
+createRoot(root).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);

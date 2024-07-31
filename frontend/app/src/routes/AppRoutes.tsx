a6e69585ed211578f7d234415066ff5a2d0b055b
ikebata_yuki <walkurepqrt@gmail.com>
2024-08-01 08:35:31 +0900

    NotFoundの追加

--- frontend/app/src/routes/AppRoutes.tsx
+++ frontend/app/src/routes/AppRoutes.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Routes, Route } from 'react-router-dom';
-import { Home } from '@pages';
+import { Home, NotFound } from '@pages';
 
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
         <Route path="/" element={ <Home/> } />
+
+        {/*どのルーティングにも当てはまらないとき*/}
+        {/* NotFoundは一番最後に置くこと。 */}
+        <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };

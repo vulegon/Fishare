988d441f56c98ea80f00fb32578060b7fada953b
ikebata_yuki <walkurepqrt@gmail.com>
2024-08-03 13:00:44 +0900

    SideBarを修正

--- frontend/app/src/features/sideBar/SideBar.tsx
+++ frontend/app/src/features/sideBar/SideBar.tsx
@@ -5,13 +5,54 @@ import {
   ListItemButton,
   ListItemText,
   ListItemIcon,
-  Box,
-  Drawer
 } from '@mui/material';
 import MapIcon from '@mui/icons-material/Map';
 import ListIcon from '@mui/icons-material/List';
 import { CustomToolbar } from "components/common";
 import { DRAWER_WIDTH } from "constants/index";
+import { styled, Theme, CSSObject } from '@mui/material/styles';
+import MuiDrawer from '@mui/material/Drawer';
+
+// MUIのDrawerコンポーネントをカスタマイズする
+// 参考
+// https://mui.com/material-ui/react-drawer/
+const openedMixin = (theme: Theme): CSSObject => ({
+  width: DRAWER_WIDTH,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const Drawer = styled(MuiDrawer,{ shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    width: DRAWER_WIDTH,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+    boxSizing: 'border-box',
+    ...(open && {
+      ...openedMixin(theme),
+      '& .MuiDrawer-paper': openedMixin(theme),
+    }),
+    ...(!open && {
+      ...closedMixin(theme),
+      '& .MuiDrawer-paper': closedMixin(theme),
+    }),
+  }),
+);
 
 const sideBarItems = [
   { label: '一覧から探す', icon: <ListIcon /> },
@@ -19,46 +60,38 @@ const sideBarItems = [
 ];
 
 interface SideBarProps {
-  isDrawerOpen: boolean;
+  open: boolean;
 }
 
-const SideBar: React.FC<SideBarProps> = (
-  { isDrawerOpen }
-) => {
+const SideBar: React.FC<SideBarProps> = ({ open }) => {
   return (
-    <>
-      <Drawer
-        open={isDrawerOpen}
-        variant="persistent"
-        sx={{
-          width: DRAWER_WIDTH,
-          flexShrink: 0,
-          [`& .MuiDrawer-paper`]: {
-            width:  isDrawerOpen ? DRAWER_WIDTH : 56,
-            boxSizing: 'border-box',
-          },
-        }}
-      >
-        <CustomToolbar />
-        <Box
-          sx={{ width: '14rem', overflow: 'auto' }}
-        >
-          <List>
-            {sideBarItems.map((item) => (
-              <ListItem key={item.label} disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>{item.icon}</ListItemIcon>
-                  <ListItemText
-                    primary={item.label}
-                    sx={{ opacity: isDrawerOpen ? 1 : 0 }}
-                  />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-        </Box>
-      </Drawer>
-    </>
+    <Drawer variant="permanent" open={open}>
+      <CustomToolbar />
+      <List>
+        {sideBarItems.map((item) => (
+          <ListItem key={item.label} disablePadding>
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? 'initial' : 'center',
+                px: 2.5,
+              }}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : 'auto',
+                  justifyContent: 'center',
+                }}
+              >
+                {item.icon}
+              </ListItemIcon>
+              <ListItemText primary={item.label} sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
   );
 };
 

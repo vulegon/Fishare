ce9806affdb2ff876a989a1d2036bb07a99b5ae5
ikebata_yuki <walkurepqrt@gmail.com>
2024-08-03 12:22:15 +0900

    header修正

--- frontend/app/src/features/header/Header.tsx
+++ frontend/app/src/features/header/Header.tsx
@@ -17,7 +17,7 @@ const Header: React.FC<HeaderProps> = ({
 }) => {
   return (
     <>
-      <AppBar position="fixed" sx={{zIndex: (theme) => theme.zIndex.drawer + 1}}>
+      <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
         <CustomToolbar>
           <IconButton
             color="inherit"

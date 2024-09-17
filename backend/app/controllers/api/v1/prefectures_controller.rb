fab74004c0381be0deeae82590811905f0fcabff
ikebata_yuki <walkurepqrt@gmail.com>
2024-09-17 20:51:46 +0900

    rubocop修正

--- backend/app/controllers/api/v1/prefectures_controller.rb
+++ backend/app/controllers/api/v1/prefectures_controller.rb
@@ -5,8 +5,6 @@ module Api
         prefectures = Prefecture.all.sort_by{ |prefecture| ::Prefecture::NAMES.index(prefecture.name) }
         serialized_prefectures = ActiveModelSerializers::SerializableResource.new(prefectures, each_serializer: ::Api::V1::PrefectureSerializer).as_json
 
-        a = 1
-
         json = { message: '都道府県を取得しました', prefectures: serialized_prefectures }
         render json: json, status: :ok
       end

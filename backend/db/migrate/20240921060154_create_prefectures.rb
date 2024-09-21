817c8771f5da192c5d4b16f244a1901e6d689daa
ikebata_yuki <walkurepqrt@gmail.com>
2024-09-21 15:02:56 +0900

    prefectures作成

--- backend/db/migrate/20240921060154_create_prefectures.rb
+++ backend/db/migrate/20240921060154_create_prefectures.rb
@@ -3,6 +3,7 @@ class CreatePrefectures < ActiveRecord::Migration[7.0]
     create_table :prefectures, id: :uuid do |t|
       t.string :name, null: false, comment: '都道府県名'
       t.integer :sort, null: false, comment: '並び順'
+
       t.timestamps
     end
   end

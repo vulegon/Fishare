0a21de59ce7d9def5d18a08da94e9d3783b0aecc
ikebata_yuki <walkurepqrt@gmail.com>
2024-09-21 18:51:38 +0900

    URLを設定

--- frontend/app/src/pages/PrefectureSpots.tsx
+++ frontend/app/src/pages/PrefectureSpots.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Typography } from '@mui/material';
 import { useParams } from 'react-router-dom';
+import { MainLayout } from 'features/layouts';
 
 export const PrefectureSpots: React.FC = () => {
   const { prefecture_id } = useParams();
   return (
     <>
-      <Typography variant="h1">PrefectureSpots{prefecture_id}</Typography>
+      <MainLayout >
+        <Typography variant="h1">PrefectureSpots{prefecture_id}</Typography>
+      </MainLayout >
     </>
   );
 };

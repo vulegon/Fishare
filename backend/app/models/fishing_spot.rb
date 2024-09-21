3813e332b8b836c538c1e6f405bacdf2bb7ced56
ikebata_yuki <walkurepqrt@gmail.com>
2024-09-21 19:16:56 +0900

    fishing_spotとfactory_botを追加

---
+++ backend/app/models/fishing_spot.rb
@@ -0,0 +1,2 @@
+class FishingSpot < ApplicationRecord
+end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_21_060154) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_21_095234) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "fishing_spots", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "prefectures", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false, comment: "都道府県名"

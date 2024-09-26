28a063c2d0fe3951040b9fabab24894311bf5688
ikebata_yuki <walkurepqrt@gmail.com>
2024-09-26 21:53:55 +0900

    factory実装

---
+++ backend/app/factory_methods/supports/contact_image_factory.rb
@@ -0,0 +1,29 @@
+module Supports
+  class ContactImageFactory
+    # お問い合わせ画像のデータを作成します。永続化は行いません。
+    # @param param [Hash] お問い合わせ画像作成パラメータ
+    # @param contact_id [String] お問い合わせID
+    def build(param, contact_id)
+      image_id = SecureRandom.uuid
+      ::Supports::ContactImage.new(
+        id: image_id,
+        support_contact_id: contact_id,
+        s3_key: param[:s3_key],
+        s3_url: param[:s3_url],
+        file_name: param[:file_name],
+        content_type: param[:content_type],
+        file_size: param[:file_size],
+      )
+    end
+
+    # 複数のお問い合わせ画像のデータを作成します。永続化は行いません。
+    # @param image_params [Array<Hash>] お問い合わせ画像作成パラメータ
+    # @param contact_id [String] お問い合わせID
+    # @return [Array<Supports::ContactImage>]
+    def build_all(image_params, contact_id)
+      image_params.map do |image_param|
+        build(image_param, contact_id)
+      end
+    end
+  end
+end
---
+++ backend/spec/factory_methods/supports/contact_image_factory_spec.rb
@@ -0,0 +1,79 @@
+require 'rails_helper'
+
+RSpec.describe Supports::ContactImageFactory do
+  describe '#build' do
+    subject { described_class.new.build(image_params, contact_id) }
+    let(:image_params) {
+      {
+        s3_key: 's3_key',
+        s3_url: 's3_url',
+        file_name: 'file_name',
+        content_type: 'content_type',
+        file_size: 100
+      }
+    }
+    let(:contact_id) { SecureRandom.uuid }
+
+    it '永続化されていないcontac_imageが返ってくること' do
+      expect(subject).to be_a(Supports::ContactImage)
+      expect(subject.id).to be_present
+      expect(subject).to have_attributes(
+        support_contact_id: contact_id,
+        s3_key: image_params[:s3_key],
+        s3_url: image_params[:s3_url],
+        file_name: image_params[:file_name],
+        content_type: image_params[:content_type],
+        file_size: image_params[:file_size]
+      )
+      expect(subject).not_to be_persisted
+    end
+  end
+
+  describe '#build_all' do
+    subject { described_class.new.build_all(image_params, contact_id) }
+    let(:image_params) {
+      [
+        {
+          s3_key: 's3_key',
+          s3_url: 's3_url',
+          file_name: 'file_name',
+          content_type: 'content_type',
+          file_size: 100
+        },
+        {
+          s3_key: 's3_key2',
+          s3_url: 's3_url2',
+          file_name: 'file_name2',
+          content_type: 'content_type2',
+          file_size: 200
+        },
+      ]
+    }
+    let(:contact_id) { SecureRandom.uuid }
+
+    it '永続化されていないcontac_imageの配列が返ってくること' do
+      expect(subject).to all(be_a(Supports::ContactImage))
+      expect(subject.size).to eq(2)
+      expect(subject[0].id).to be_present
+      expect(subject[0]).to have_attributes(
+        support_contact_id: contact_id,
+        s3_key: image_params[0][:s3_key],
+        s3_url: image_params[0][:s3_url],
+        file_name: image_params[0][:file_name],
+        content_type: image_params[0][:content_type],
+        file_size: image_params[0][:file_size]
+      )
+      expect(subject[0]).not_to be_persisted
+      expect(subject[1].id).to be_present
+      expect(subject[1]).to have_attributes(
+        support_contact_id: contact_id,
+        s3_key: image_params[1][:s3_key],
+        s3_url: image_params[1][:s3_url],
+        file_name: image_params[1][:file_name],
+        content_type: image_params[1][:content_type],
+        file_size: image_params[1][:file_size]
+      )
+      expect(subject[1]).not_to be_persisted
+    end
+  end
+end

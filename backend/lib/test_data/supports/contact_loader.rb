ac5e40e744748e1c9583cd0e4e59c34acf034dd7
ikebata_yuki <walkurepqrt@gmail.com>
2024-09-27 07:28:49 +0900

    loader修正

--- backend/lib/test_data/supports/contact_loader.rb
+++ backend/lib/test_data/supports/contact_loader.rb
@@ -4,13 +4,14 @@ module TestData
       class << self
         def load
           10.times do |i|
-            contact = ::Supports::Contact.create!(name: "test#{i}", email: "test#{i}@example.com", content: "content_sample#{i}")
+            contact = ::Supports::Contact.create!(name: Faker::Name.name, email: Faker::Internet.email, content: Faker::Lorem.sentence)
             2.times do |j|
+              random_number = rand(1..1000000000)
               ::Supports::ContactImage.create!(
                 support_contact_id: contact.id,
-                s3_key: "supports/contact/#{SecureRandom.uuid}/image#{j}.jpg",
-                s3_url: "https://s3-ap-northeast-1.amazonaws.com/supports/contact/#{SecureRandom.uuid}/image#{j}.jpg",
-                file_name: "image#{j}.jpg",
+                s3_key: "supports/contact/#{SecureRandom.uuid}/image#{random_number}.jpg",
+                s3_url: "https://s3-ap-northeast-1.amazonaws.com/supports/contact/#{SecureRandom.uuid}/image#{random_number}.jpg",
+                file_name: "image#{random_number}.jpg",
                 content_type: "image/jpeg",
                 file_size: 1024
               )

b66d4490254c5b0d54ca514b4d645a93d896a16b
ikebata_yuki <walkurepqrt@gmail.com>
2024-09-29 12:48:15 +0900

    Revert "devise追加"
    This reverts commit 0fda3bf054315072501c965d80728961f985091d.

--- backend/db/migrate/20240928132530_create_user_roles.rb
+++
@@ -1,8 +0,0 @@
-class CreateUserRoles < ActiveRecord::Migration[7.0]
-  def change
-    create_table :user_roles, id: :uuid do |t|
-      t.integer :role, null: false, comment: '権限'
-      t.timestamps
-    end
-  end
-end

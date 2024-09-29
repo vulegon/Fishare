f6ae19b200395e22d8e067274b1016f34ad3519c
ikebata_yuki <walkurepqrt@gmail.com>
2024-09-29 13:33:42 +0900

    不要な行削除

--- backend/spec/rails_helper.rb
+++ backend/spec/rails_helper.rb
@@ -19,5 +19,4 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::IntegrationHelpers, type: :request
-
 end

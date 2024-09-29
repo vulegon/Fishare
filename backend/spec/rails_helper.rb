95a278c108fda41ee3941c9edd641cfeaee196bd
ikebata_yuki <walkurepqrt@gmail.com>
2024-09-29 10:03:23 +0900

    helper修正

--- backend/spec/rails_helper.rb
+++ backend/spec/rails_helper.rb
@@ -5,11 +5,13 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
+
 RSpec.configure do |config|
   config.fixture_path = Rails.root.join('spec/fixtures')
   config.use_transactional_fixtures = true

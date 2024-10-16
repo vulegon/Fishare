d3ca0a724a63a1fbf6bc48d72ecd5080aa3ae5b3
ikebata_yuki <walkurepqrt@gmail.com>
2024-10-17 08:01:48 +0900

    釣り場フォーム途中

--- frontend/app/src/api/lib/libGoogle/geocodeClient.ts
+++ frontend/app/src/api/lib/libGoogle/geocodeClient.ts
@@ -5,7 +5,7 @@ const GOOGLE_MAPS_API_KEY = process.env.REACT_APP_GOOGLE_MAP_API_KEY;
 export const fetchAddress = async(
   lat: number,
   lng: number
-): Promise<string> => {
+): Promise<{ prefecture: string, address: string }> => {
     try {
       const response = await axios.get(
         `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${GOOGLE_MAPS_API_KEY}`,
@@ -18,11 +18,15 @@ export const fetchAddress = async(
       )
 
       const formattedAddress = response.data.results[0].formatted_address;
-      const cleanedAddress = formattedAddress
+      const fullAddress = formattedAddress
       .replace(/^日本[,、\s]*〒?\s*\d{3}-\d{4}\s*/, '') // 日本、〒マーク＋郵便番号を削除
       .replace(/^\s+|\s+$/g, ''); // 前後の余白を削除
 
-      return cleanedAddress;
+      const prefecture = response.data.results[0].address_components.find((component: any) =>
+        component.types.includes('administrative_area_level_1')
+      );
+
+      return {prefecture: prefecture, address: fullAddress};
     } catch (error) {
       notifyError(error);
       throw error;
--- frontend/app/src/pages/admin/fishingSpots/FishingSpotNewPage.tsx
+++ frontend/app/src/pages/admin/fishingSpots/FishingSpotNewPage.tsx
@@ -9,8 +9,27 @@ import {
 import { MainLayout } from 'features/layouts';
 import Container from '@mui/material/Container';
 import { useSearchParams } from 'react-router-dom';
-import { FishingSpotNewLoadMap } from 'features/admin/fishingSpots/map/new/FishingSpotNewLoadMap';
+import {
+  FishingSpotNewLoadMap,
+  Label
+} from 'features/admin/fishingSpots/map/new/';
 import { fetchAddress } from 'api/lib/libGoogle/geocodeClient';
+import { Prefecture } from 'interfaces/api';
+import apiClient from 'api/v1/apiClient';
+import RoomIcon from '@mui/icons-material/Room';
+import InfoIcon from '@mui/icons-material/Info';
+import MapIcon from '@mui/icons-material/Map';
+import { faFish } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import MyLocationIcon from '@mui/icons-material/MyLocation';
+
+const spotLabels = [
+  { label: '釣り場の名前', icon: <MyLocationIcon/>  },
+  { label: '説明', icon: <InfoIcon/>  },
+  { label: '住所', icon: <RoomIcon/>  },
+  { label: '釣れる魚', icon: <FontAwesomeIcon icon={faFish}/>  },
+  { label: '釣り場の場所', icon: <MapIcon/>  },
+]
 
 export const FishingSpotNewPage: React.FC = () => {
   const [searchParams] = useSearchParams();
@@ -18,14 +37,23 @@ export const FishingSpotNewPage: React.FC = () => {
   const lng = searchParams.get('lng') ? parseFloat(searchParams.get('lng')!) : 139.767125;
   const [marker, setMarker] = useState<google.maps.LatLngLiteral>({ lat: lat, lng: lng });
   const [address, setAddress] = useState<string>('');
+  const [prefecture, setPrefecture] = useState<Prefecture>();
 
   useEffect(() => {
     const fetch = async () => {
-      const address = await fetchAddress(marker.lat, marker.lng);
-      setAddress(address);
+      const addressResponse = await fetchAddress(marker.lat, marker.lng);
+      const prefectureData = await apiClient.getPrefectures();
+      const findPrefecture = prefectureData.prefectures.find((pref: Prefecture)=> pref.name === addressResponse.prefecture);
+
+      setAddress(addressResponse.address);
+      setPrefecture(findPrefecture);
     };
+
     fetch();
-  }, [marker]);
+  }, [marker, prefecture]);
+
+  useEffect(() => {
+  }, [prefecture]);
 
   const onMapClick = useCallback((e: google.maps.MapMouseEvent) => {
     if (!e.latLng) return;
@@ -34,7 +62,12 @@ export const FishingSpotNewPage: React.FC = () => {
 
   return (
     <MainLayout>
-      <Container fixed sx={{ mb: 6 }}>
+      <Container
+        fixed
+        sx={{
+          mb: 6,
+        }}
+      >
       <Box
         sx={{
           display: 'flex',
@@ -48,44 +81,19 @@ export const FishingSpotNewPage: React.FC = () => {
         <Typography variant="h4" gutterBottom sx={{ fontWeight: 'bold' }}>
           釣り場登録
         </Typography>
-        <Typography variant="subtitle2" gutterBottom sx={{ color: 'text.secondary' }}>
-          地図のマーカーを動かして釣り場の位置を調整してください。
-        </Typography>
       </Box>
 
         <Stack spacing={5}>
-          {/* 地図コンポーネント */}
-          <Box
-            sx={{
-              borderRadius: 2,
-              overflow: 'hidden',
-              boxShadow: 3,
-              width: '100%',
-              height: '400px',
-              minWidth: '500px',
-            }}
-          >
-            <FishingSpotNewLoadMap
-              marker={marker}
-              onMapClick={onMapClick}
-            />
-          </Box>
-
-          {/* フォーム入力フィールド */}
           <Stack spacing={3}>
             {/** 名称入力 **/}
             <Box>
-              <Typography variant="body1" sx={{ fontWeight: 'bold', mb: 1 }}>
-                名称
-              </Typography>
+              <Label label={'釣り場の名前'} icon={<MyLocationIcon/>}/>
               <TextField label="名称を入力" variant="outlined" fullWidth />
             </Box>
 
             {/** 住所入力 **/}
             <Box>
-              <Typography variant="body1" sx={{ fontWeight: 'bold', mb: 1 }}>
-                住所
-              </Typography>
+              <Label label={'住所'} icon={<RoomIcon/>}/>
               <TextField
                 label="住所を入力"
                 variant="outlined"
@@ -96,9 +104,10 @@ export const FishingSpotNewPage: React.FC = () => {
 
             {/** 釣れる魚入力 **/}
             <Box>
-              <Typography variant="body1" sx={{ fontWeight: 'bold', mb: 1 }}>
-                釣れる魚
-              </Typography>
+              <Label
+                label={'釣れる魚'}
+                icon={<FontAwesomeIcon icon={faFish} style={{ fontSize: '23px' }}/>}
+              />
               <TextField
                 label="釣れる魚を入力"
                 variant="outlined"
@@ -108,11 +117,8 @@ export const FishingSpotNewPage: React.FC = () => {
               />
             </Box>
 
-            {/** 備考入力 **/}
             <Box>
-              <Typography variant="body1" sx={{ fontWeight: 'bold', mb: 1 }}>
-                備考
-              </Typography>
+              <Label label={'説明'} icon={<InfoIcon/>}/>
               <TextField
                 label="備考を入力"
                 variant="outlined"
@@ -120,6 +126,25 @@ export const FishingSpotNewPage: React.FC = () => {
                 multiline
                 rows={3}
               />
+            </Box>
+
+            <Box>
+              <Label label={'釣り場の場所'} icon={<MapIcon/>}/>
+              <Box
+                sx={{
+                  borderRadius: 2,
+                  overflow: 'hidden',
+                  boxShadow: 3,
+                  width: '100%',
+                  height: '400px',
+                  minWidth: '500px',
+                }}
+              >
+                <FishingSpotNewLoadMap
+                  marker={marker}
+                  onMapClick={onMapClick}
+                />
+              </Box>
             </Box>
           </Stack>
 

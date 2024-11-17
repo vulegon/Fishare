a7a92b3ca7d188f99880328e8150f319f27a2d31
ikebata_yuki <walkurepqrt@gmail.com>
2024-11-18 07:47:49 +0900

    helperの修正

--- backend/spec/models/fish_spec.rb
+++ backend/spec/models/fish_spec.rb
@@ -14,5 +14,30 @@
 require 'rails_helper'
 
 RSpec.describe Fish, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    subject { fish.valid? }
+
+    shared_examples '無効な場合の検証' do |error_message|
+      it 'falseを返すこと' do
+        is_expected.to eq false
+      end
+
+      it '適切なエラーメッセージを返すこと' do
+        subject
+        expect(fish.errors.full_messages).to include(error_message)
+      end
+    end
+
+    context 'nameが誤りのとき' do
+      context 'nameがnilのとき' do
+        let(:fish) { build(:fish, name: nil) }
+        it_behaves_like '無効な場合の検証', '魚の名前を入力してください'
+      end
+
+      context 'nameが空文字のとき' do
+        let(:fish) { build(:fish, name: '') }
+        it_behaves_like '無効な場合の検証', '魚の名前を入力してください'
+      end
+    end
+  end
 end

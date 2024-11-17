d613d973accb57fc17af3672e8bd27eca14d1c7b
ikebata_yuki <walkurepqrt@gmail.com>
2024-11-17 09:28:17 +0900

    dependentを追加

--- backend/app/models/fishing_spot.rb
+++ backend/app/models/fishing_spot.rb
@@ -10,7 +10,7 @@
 #
 # 釣り場を表すテーブル
 class FishingSpot < ApplicationRecord
-  has_many :fishing_spot_fishes
+  has_many :fishing_spot_fishes, dependent: :destroy, inverse_of: :fishing_spot
   has_many :fishes, through: :fishing_spot_fishes
   has_many :locations, class_name: 'FishingSpotLocation', dependent: :destroy, inverse_of: :fishing_spot
   has_many :images, class_name: 'FishingSpotImage', dependent: :destroy, inverse_of: :fishing_spot

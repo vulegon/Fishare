8be7e3ac67730b1a13ef5bfadcf86fe50d8e8095
ikebata_yuki <walkurepqrt@gmail.com>
2024-11-22 06:44:35 +0900

    作業途中

--- frontend/app/src/components/fishingSpots/googleMap/FishingSpotShowView.tsx
+++ frontend/app/src/components/fishingSpots/googleMap/FishingSpotShowView.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { Drawer, Divider } from "@mui/material";
+import React, { useEffect, useState, useCallback } from "react";
+import { Drawer, Divider, Box, Stack, Typography } from "@mui/material";
 import { streetViewClient } from "api/lib/libGoogle/streetViewClient";
-import { FishingSpotLocation } from "interfaces/api";
+import { FishingSpotLocation, FishingSpot } from "interfaces/api";
 import { CenteredLoader } from "components/common";
 import apiClient from "api/v1/apiClient";
+import { styled } from "@mui/material/styles";
 
 interface FishingSpotShowViewProps {
   selectedLocation: FishingSpotLocation | null;
@@ -12,21 +13,39 @@ interface FishingSpotShowViewProps {
 
 const DRAWER_WIDTH = "500px";
 
+const FishingSpotBox = styled(Box)({
+  marginLeft: 2
+});
+
 export const FishingSpotShowView: React.FC<FishingSpotShowViewProps> = ({
   selectedLocation,
   onClose,
 }) => {
   const [streetViewImageUrl, setStreetViewImageUrl] = useState<string | null>(null);
-  const [streetViewImageLoaded, setStreetViewImageLoaded] = useState<boolean>(false);
-  const fetchStreetViewImage = async () => {
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const [fishingSpot, setFishingSpot] = useState<FishingSpot | null>(null);
+
+  const fetchStreetViewImage = useCallback(async () => {
     if (!selectedLocation) return;
     const response = await streetViewClient.fetchStreetViewImage(selectedLocation.latitude, selectedLocation.longitude);
     setStreetViewImageUrl(response);
-    setStreetViewImageLoaded(true);
-  };
+  }, [selectedLocation]);
+
+  const fetchFishingSpot = useCallback(async () => {
+    if (!selectedLocation) return;
+    const response = await apiClient.showFishingSpot(selectedLocation.id);
+    setFishingSpot(response);
+  }, [selectedLocation]);
 
   useEffect(() => {
-    fetchStreetViewImage();
+    if (!selectedLocation) return;
+
+    const fetchData = async () => {
+      await Promise.all([fetchStreetViewImage(), fetchFishingSpot()]);
+      setIsLoaded(true);
+    };
+
+    fetchData();
   }, [selectedLocation]);
   return (
     <Drawer
@@ -48,14 +67,33 @@ export const FishingSpotShowView: React.FC<FishingSpotShowViewProps> = ({
         onClose();
       }}
     >
-      { (streetViewImageUrl && streetViewImageLoaded) ?
-        (
-          <img src={streetViewImageUrl} alt="street view" />
-        ) : (
-          <CenteredLoader />
-        )
-      }
-      <Divider />
+      <Stack spacing={2} useFlexGap>
+        <Box
+          sx={{
+            height: '400px',
+          }}
+        >
+          {
+            (isLoaded) ?
+              (
+                <img src={streetViewImageUrl ?? undefined} alt="street view image" />
+              ) : (
+                <CenteredLoader/>
+            )
+          }
+        </Box>
+        <FishingSpotBox>
+          <Typography variant="h5" sx={{fontWeight: 600}}>
+            釣り場情報
+          </Typography>
+        </FishingSpotBox>
+        <Divider />
+        <FishingSpotBox>
+          <Typography variant="subtitle1">
+            {fishingSpot?.name}
+          </Typography>
+        </FishingSpotBox>
+      </Stack>
     </Drawer>
   );
 };

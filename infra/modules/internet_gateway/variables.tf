1e8cbcbf8921daf8e06e4d95f42bdb3a0791c711
ikebata_yuki <walkurepqrt@gmail.com>
2024-12-05 11:00:56 +0900

    internet_gateway

---
+++ infra/modules/internet_gateway/variables.tf
@@ -0,0 +1,14 @@
+variable "vpc_id" {
+  description = "VPCのID"
+  type        = string
+}
+
+variable  "env" {
+  description = "環境名"
+  type        = string
+}
+
+variable "product_name" {
+  description = "プロダクト名"
+  type        = string
+}

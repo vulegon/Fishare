ddde61d336aa1864bd3a0abe99c4c6588bcc695d
ikebata_yuki <walkurepqrt@gmail.com>
2024-12-14 15:14:57 +0900

    security_group修正

--- infra/modules/security_group/main.tf
+++ infra/modules/security_group/main.tf
@@ -132,18 +132,12 @@ resource "aws_security_group" "nat_instance_sg" {
   }
 
   egress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # すべてのIPアドレスからHTTPを許可
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # すべてのIPアドレスからHTTPSを許可
-  }
   tags = {
     Name = "${var.env}-${var.product_name}-nat-instance-sg"
   }

d4b37d2dc3ac209f0da2f25c3ba406e8a0a4641d
ikebata_yuki <walkurepqrt@gmail.com>
2024-12-28 06:42:14 +0900

    generate_serialize作成

---
+++ api/app/serializers/api/v1/supports/contact_images/generate_presigned_url_serializer.rb
@@ -0,0 +1,26 @@
+module Api
+  module V1
+    module Supports
+      module ContactImages
+        class GeneratePresignedUrlSerializer
+          def initialize(image_form)
+            @image_form = image_form
+            @s3_helper = ::LibAws::S3Helper.new
+            @s3_key = @s3_helper.generate_s3_key(prefix: 'supports/contact_images', file_name: @image_form.file_name)
+          end
+
+          def as_json
+            {
+              url: @s3_helper.put_presigned_url(
+                bucket_name: Rails.configuration.x.lib_aws.s3[:support_contact_image_bucket],
+                key: @s3_key,
+                content_type: @image_form.content_type
+              ),
+              s3_key: @s3_key,
+            }
+          end
+        end
+      end
+    end
+  end
+end
---
+++ api/spec/serializers/api/v1/supports/contact_images/generate_presigned_url_serializer_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe ::Api::V1::Supports::ContactImages::GeneratePresignedUrlSerializer, type: :serializer do
+  describe '#as_json' do
+    subject { described_class.new(image_form).as_json }
+    let(:image_form) { ::Api::V1::Supports::ContactImages::ImageForm.new(params) }
+    let(:params) {{ file_name: 'test.jpg', content_type: 'image/jpeg' }}
+    let(:random_uuid) { SecureRandom.uuid }
+    before do
+      allow(SecureRandom).to receive(:uuid).and_return(random_uuid)
+    end
+
+    it '整形されたデータが返ってくること' do
+      expect(subject[:s3_key]).to eq("supports/contact_images/#{random_uuid}/test.jpg")
+      expect(subject[:url]).to a_string_including("supports/contact_images/#{random_uuid}/test.jpg")
+    end
+  end
+end

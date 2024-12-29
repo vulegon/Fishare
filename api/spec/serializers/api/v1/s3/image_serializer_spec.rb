e7491afeeb7471e6832b716bd31c6a3c3734ad41
ikebata_yuki <walkurepqrt@gmail.com>
2024-12-30 08:08:42 +0900

    image_serializer修正

--- api/spec/serializers/api/v1/s3/image_serializer_spec.rb
+++ api/spec/serializers/api/v1/s3/image_serializer_spec.rb
@@ -6,20 +6,7 @@ RSpec.describe ::Api::V1::S3::ImageSerializer, type: :serializer do
 
     let(:parameter) { ::Api::V1::S3::PresignedUrlParameter.new(ActionController::Parameters.new(params)) }
     let(:prefix) { 'supports/contact_images' }
-    let(:params) {
-      {
-        images: [
-          {
-            file_name: 'サンプル1.jpg',
-            content_type: 'image/jpeg'
-          },
-          {
-            file_name: 'サンプル2.jpg',
-            content_type: 'image/jpeg'
-          }
-        ]
-      }
-    }
+
     let(:random_uuid) { SecureRandom.uuid }
 
     before do
@@ -27,11 +14,40 @@ RSpec.describe ::Api::V1::S3::ImageSerializer, type: :serializer do
       parameter.valid?
     end
 
-    it '整形されたデータが返ってくること' do
-      expect(subject[:images].first[:s3_key]).to eq("supports/contact_images/#{random_uuid}/サンプル1.jpg")
-      expect(subject[:images].first[:presigned_url]).to be_present
-      expect(subject[:images].last[:s3_key]).to eq("supports/contact_images/#{random_uuid}/サンプル2.jpg")
-      expect(subject[:images].last[:presigned_url]).to be_present
+    context "parameterのimage_formsが存在するとき" do
+      let(:params) {
+        {
+          images: [
+            {
+              file_name: 'サンプル1.jpg',
+              content_type: 'image/jpeg'
+            },
+            {
+              file_name: 'サンプル2.jpg',
+              content_type: 'image/jpeg'
+            }
+          ]
+        }
+      }
+
+      it '整形されたデータが返ってくること' do
+        expect(subject[:images].first[:s3_key]).to eq("supports/contact_images/#{random_uuid}/サンプル1.jpg")
+        expect(subject[:images].first[:presigned_url]).to be_present
+        expect(subject[:images].last[:s3_key]).to eq("supports/contact_images/#{random_uuid}/サンプル2.jpg")
+        expect(subject[:images].last[:presigned_url]).to be_present
+      end
+    end
+
+    context "parameterのimage_formsが存在しないとき" do
+      let(:params) {
+        {
+          images: []
+        }
+      }
+
+      it '整形されたデータが返ってくること' do
+        expect(subject[:images]).to be_empty
+      end
     end
   end
 end

6f7e990e4b6cb606659a3ab6023e8195f3cccfa7
ikebata_yuki <walkurepqrt@gmail.com>
2024-12-30 11:14:41 +0900

    factory修正

--- api/app/factory_methods/fishing_spot_image_factory.rb
+++ api/app/factory_methods/fishing_spot_image_factory.rb
@@ -1,17 +1,25 @@
 class FishingSpotImageFactory
+  # 釣り場画像を生成する。永続化はしません。
+  # @param [Api::V1::FishingSpots::ImageForm] param
+  # @param [String] fishing_spot_id
+  # @return [FishingSpotImage]
   def build(param, fishing_spot_id)
     image_id = SecureRandom.uuid
     ::FishingSpotImage.new(
       id: image_id,
       fishing_spot_id: fishing_spot_id,
-      s3_key: param[:s3_key],
-      file_name: param[:file_name],
-      content_type: param[:content_type],
-      file_size: param[:file_size],
-      display_order: param[:display_order].to_i
+      s3_key: param.s3_key,
+      file_name: param.file_name,
+      content_type: param.content_type,
+      file_size: param.file_size,
+      display_order: param.display_order.to_i
     )
   end
 
+  # 釣り場画像を複数生成する。永続化はしません。
+  # @param [Array<Api::V1::FishingSpots::ImageForm>] image_params
+  # @param [String] fishing_spot_id
+  # @return [Array<FishingSpotImage>]
   def build_all(image_params, fishing_spot_id)
     image_params.map do |image_param|
       build(image_param, fishing_spot_id)
--- api/spec/factory_methods/fishing_spot_image_factory_spec.rb
+++ api/spec/factory_methods/fishing_spot_image_factory_spec.rb
@@ -4,74 +4,98 @@ RSpec.describe FishingSpotImageFactory do
   describe '#build' do
     subject { described_class.new.build(image_params, fishing_spot_id) }
     let(:image_params) {
-      {
-        s3_key: 's3_key',
-        file_name: 'file_name',
-        content_type: 'content_type',
-        file_size: 100
-      }
+      ::Api::V1::FishingSpots::ImageForm.new(
+        s3_key: 'S3キー',
+        file_name:  'ファイル名',
+        content_type: 'ファイルの拡張子',
+        file_size: 10,
+        display_order: 1
+      )
     }
     let(:fishing_spot_id) { SecureRandom.uuid }
 
-    it '永続化されていないimageが返ってくること' do
+    it '戻り値がFishingSpotImageであること' do
       expect(subject).to be_a(FishingSpotImage)
+    end
+
+    it '永続化されていないimageが返ってくること' do
+      expect(subject).not_to be_persisted
+    end
+
+    it 'FishingSpotImageが期待値の内容であること' do
       expect(subject.id).to be_present
       expect(subject).to have_attributes(
         fishing_spot_id: fishing_spot_id,
-        s3_key: image_params[:s3_key],
-        file_name: image_params[:file_name],
-        content_type: image_params[:content_type],
-        file_size: image_params[:file_size]
+        s3_key: image_params.s3_key,
+        file_name: image_params.file_name,
+        content_type: image_params.content_type,
+        file_size: image_params.file_size
       )
-      expect(subject).not_to be_persisted
     end
   end
 
   describe '#build_all' do
     subject { described_class.new.build_all(image_params, fishing_spot_id) }
+
+    let(:image_form_1) {
+      ::Api::V1::FishingSpots::ImageForm.new(
+        s3_key: 'S3キー',
+        file_name:  'ファイル名',
+        content_type: 'ファイルの拡張子',
+        file_size: 10,
+        display_order: 1
+      )
+    }
+    let(:image_form_2) {
+      ::Api::V1::FishingSpots::ImageForm.new(
+        s3_key: 'S3キー2',
+        file_name:  'ファイル名2',
+        content_type: 'ファイルの拡張子',
+        file_size: 10,
+        display_order: 2
+      )
+    }
     let(:image_params) {
-      [
-        {
-          s3_key: 's3_key',
-          file_name: 'file_name',
-          content_type: 'content_type',
-          file_size: 100,
-          display_order: 1
-        },
-        {
-          s3_key: 's3_key2',
-          file_name: 'file_name2',
-          content_type: 'content_type2',
-          file_size: 200,
-          display_order: 2
-        },
-      ]
+      [image_form_1, image_form_2]
     }
     let(:fishing_spot_id) { SecureRandom.uuid }
 
-    it '永続化されていないimageの配列が返ってくること' do
+    it '戻り値がFishingSpotImageの配列であること' do
       expect(subject).to all(be_a(FishingSpotImage))
-      expect(subject.size).to eq(2)
-      expect(subject[0].id).to be_present
-      expect(subject[0]).to have_attributes(
-        fishing_spot_id: fishing_spot_id,
-        s3_key: image_params[0][:s3_key],
-        file_name: image_params[0][:file_name],
-        content_type: image_params[0][:content_type],
-        file_size: image_params[0][:file_size],
-        display_order: image_params[0][:display_order]
-      )
-      expect(subject[0]).not_to be_persisted
-      expect(subject[1].id).to be_present
-      expect(subject[1]).to have_attributes(
-        fishing_spot_id: fishing_spot_id,
-        s3_key: image_params[1][:s3_key],
-        file_name: image_params[1][:file_name],
-        content_type: image_params[1][:content_type],
-        file_size: image_params[1][:file_size],
-        display_order: image_params[1][:display_order]
-      )
-      expect(subject[1]).not_to be_persisted
+    end
+
+    describe '戻り値1つ目に関して' do
+      it '永続化されていないimageが返ってくること' do
+        expect(subject[0]).not_to be_persisted
+      end
+
+      it 'FishingSpotImageが期待値の内容であること' do
+        expect(subject[0].id).to be_present
+        expect(subject[0]).to have_attributes(
+          fishing_spot_id: fishing_spot_id,
+          s3_key: image_form_1.s3_key,
+          file_name: image_form_1.file_name,
+          content_type: image_form_1.content_type,
+          file_size: image_form_1.file_size
+        )
+      end
+    end
+
+    describe '戻り値2つ目に関して' do
+      it '永続化されていないimageが返ってくること' do
+        expect(subject[1]).not_to be_persisted
+      end
+
+      it 'FishingSpotImageが期待値の内容であること' do
+        expect(subject[1].id).to be_present
+        expect(subject[1]).to have_attributes(
+          fishing_spot_id: fishing_spot_id,
+          s3_key: image_form_2.s3_key,
+          file_name: image_form_2.file_name,
+          content_type: image_form_2.content_type,
+          file_size: image_form_2.file_size
+        )
+      end
     end
   end
 end

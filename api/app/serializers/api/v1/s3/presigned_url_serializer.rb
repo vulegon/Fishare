c2e4fb0416030c4b2537af5d7d495063d49faf4a
ikebata_yuki <walkurepqrt@gmail.com>
2024-12-30 09:57:34 +0900

    presign修正

--- api/app/serializers/api/v1/s3/presigned_url_serializer.rb
+++ api/app/serializers/api/v1/s3/presigned_url_serializer.rb
@@ -2,16 +2,17 @@ module Api
   module V1
     module S3
       class PresignedUrlSerializer
-        def initialize(image_form, prefix)
+        def initialize(image_form, prefix, bucket_name)
           @image_form = image_form
           @s3_helper = ::LibAws::S3Helper.new
           @s3_key = @s3_helper.generate_s3_key(prefix: prefix, file_name: @image_form.file_name)
+          @bucket_name = bucket_name.to_sym
         end
 
         def as_json
           {
             presigned_url: @s3_helper.put_presigned_url(
-              bucket_name: Rails.configuration.x.lib_aws.s3[:support_contact_image_bucket],
+              bucket_name: Rails.configuration.x.lib_aws.s3[@bucket_name],
               key: @s3_key,
               content_type: @image_form.content_type
             ),
--- api/spec/serializers/api/v1/s3/presigned_url_serializer_spec.rb
+++ api/spec/serializers/api/v1/s3/presigned_url_serializer_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 
 RSpec.describe ::Api::V1::S3::PresignedUrlSerializer, type: :serializer do
   describe '#as_json' do
-    subject { described_class.new(image_form, prefix).as_json }
+    subject { described_class.new(image_form, prefix, bucket_name).as_json }
     let(:image_form) { ::Api::V1::S3::ImageForm.new(params) }
     let(:params) {{ file_name: 'test.jpg', content_type: 'image/jpeg' }}
     let(:random_uuid) { SecureRandom.uuid }
     let(:prefix) { 'supports/contact_images' }
+    let(:bucket_name) { 'support_contact_image_bucket' }
+
     before do
       allow(SecureRandom).to receive(:uuid).and_return(random_uuid)
     end

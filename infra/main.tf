fb757ceb5f5621556f3cd2dcda9d2e5bce34042d
ikebata_yuki <walkurepqrt@gmail.com>
2025-01-05 07:29:30 +0900

    infraのコメントアウト取り消し

--- infra/main.tf
+++ infra/main.tf
@@ -106,26 +106,26 @@ module "ecr" {
   env = var.env
 }
 
-# module "ecs" {
-#   source = "./modules/ecs"
-#   product_name = var.product_name
-#   env = var.env
-#   desired_count = 1 # 個人開発のため、最小構成で構築。本番環境では適切な数を設定すること。
-#   api_service_security_group_ids = [module.security_group.api_service_id]
-#   front_service_security_group_ids = [module.security_group.front_service_id]
-#   public_subnet_ids = module.subnet.public_subnet_ids
-#   ecs_task_execution_role_arn = module.iam.ecs_task_execution_role_arn
-#   ecs_task_role_arn = module.iam.ecs_task_role_arn
-#   api_repository_url = module.ecr.api_repository_url
-#   front_repository_url = module.ecr.front_repository_url
-#   api_ecs_log_group_name = module.cloudwatch.api_ecs_log_group_name
-#   front_ecs_log_group_name = module.cloudwatch.front_ecs_log_group_name
-#   redis_ecs_log_group_name = module.cloudwatch.redis_ecs_log_group_name
-#   sidekiq_ecs_log_group_name = module.cloudwatch.sidekiq_ecs_log_group_name
-#   api_alb_target_group_arn = module.alb.api_alb_target_group_arn
-#   front_alb_target_group_arn = module.alb.front_alb_target_group_arn
-#   api_rails_master_key_arn = data.aws_ssm_parameter.rails_master_key.arn
-# }
+module "ecs" {
+  source = "./modules/ecs"
+  product_name = var.product_name
+  env = var.env
+  desired_count = 1 # 個人開発のため、最小構成で構築。本番環境では適切な数を設定すること。
+  api_service_security_group_ids = [module.security_group.api_service_id]
+  front_service_security_group_ids = [module.security_group.front_service_id]
+  public_subnet_ids = module.subnet.public_subnet_ids
+  ecs_task_execution_role_arn = module.iam.ecs_task_execution_role_arn
+  ecs_task_role_arn = module.iam.ecs_task_role_arn
+  api_repository_url = module.ecr.api_repository_url
+  front_repository_url = module.ecr.front_repository_url
+  api_ecs_log_group_name = module.cloudwatch.api_ecs_log_group_name
+  front_ecs_log_group_name = module.cloudwatch.front_ecs_log_group_name
+  redis_ecs_log_group_name = module.cloudwatch.redis_ecs_log_group_name
+  sidekiq_ecs_log_group_name = module.cloudwatch.sidekiq_ecs_log_group_name
+  api_alb_target_group_arn = module.alb.api_alb_target_group_arn
+  front_alb_target_group_arn = module.alb.front_alb_target_group_arn
+  api_rails_master_key_arn = data.aws_ssm_parameter.rails_master_key.arn
+}
 
 module "cloudwatch" {
   source = "./modules/cloud_watch"
